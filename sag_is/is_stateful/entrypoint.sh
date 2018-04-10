c8832500c5fc10de7f0b0639a445b8b1ed073ccd
fabien.sanglier <fabiensanglier@gmail.com>
2018-04-10 16:14:31 -0400

    finalized all the templates for BPMS...need to test now

--- sag_is/is_stateful/entrypoint.sh
+++ sag_is/is_stateful/entrypoint.sh
@@ -13,27 +13,20 @@ fi
 # or do this...
 
 echo "Remove old logs"
-rm -rf $SAG_HOME/profiles/IS_$INSTANCE_NAME/logs/*.log
+rm -rf $SAG_HOME/profiles/IS_$INSTANCE_NAME/logs/*
+rm -rf $SAG_HOME/IntegrationServer/instances/$INSTANCE_NAME/logs/*
 
 # or can start in background
 $SAG_HOME/profiles/IS_$INSTANCE_NAME/bin/startup.sh
 
 echo "IS process started. Waiting ..."
+
 # wait until IS server.log comes up
 while [  ! -f $SAG_HOME/IntegrationServer/instances/$INSTANCE_NAME/logs/server.log ]; do
-    # tail $SAG_HOME/profiles/IS_$INSTANCE_NAME/logs/wrapper.log
+    tail $SAG_HOME/profiles/IS_$INSTANCE_NAME/logs/wrapper.log
     sleep 5
-done
-
-echo "IS process successfully started. Waiting for HTTP stack ..."
-until curl Administrator:manage -s http://`hostname`:5555/ 
-do 
-    sleep 5
-    tail $SAG_HOME/IntegrationServer/instances/$INSTANCE_NAME/logs/server.log
 done
 
 # this is our main container process
-
 echo "Integration Server is ONLINE at http://`hostname`:5555/"
-
 tail -f $SAG_HOME/IntegrationServer/instances/$INSTANCE_NAME/logs/server.log
