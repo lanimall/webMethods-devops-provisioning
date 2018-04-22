94434b66abd391c454e7dbbabd69a13ac88c6a11
fabien.sanglier <fabiensanglier@gmail.com>
2018-04-22 19:29:00 -0400

    updated um layer with new settings and optimized entrypoint

--- templates/um-layer/entrypoint.sh
+++ templates/um-layer/entrypoint.sh
@@ -8,43 +8,44 @@ if [ -d $SAG_HOME/profiles/SPM/bin ]; then
     $SAG_HOME/profiles/SPM/bin/startup.sh
 fi
 
-# Define all of the usual realm server options, except:
-#     The realm name which is parameterised from the environment
-#     Logging which goes to stdout in order to be captured by Docker's logging system
-#     SERVER_OPTS_EXTRA from the environment
-SERVER_OPTS="-DDATADIR=server/$REALM/data
-             -DREALM=$REALM
-             -DSERVERDIR=server/$REALM
-             -DADAPTER_0=nhp://0.0.0.0:$UM_PORT
-             -DLICENCE_DIR=server/license/
-             -DLICENCE_FILE=licence.xml
-             -DLOGFILE=/dev/stdout
-             -Djavax.net.ssl.trustStore=server/$REALM/bin/nirvanacacerts.jks
-             -Djavax.net.ssl.trustStorePassword=nirvana
-             -Djavax.net.ssl.keyStore=server/$REALM/bin/server.jks
-             -Djavax.net.ssl.keyStorePassword=nirvana
-             -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol
-             -Dcom.sun.management.jmxremote
-             -Djava.library.path=lib/
-             -DLOGLEVEL=4
-             -XX:MaxDirectMemorySize=1G
-             $SERVER_OPTS_EXTRA"
+
+
+# Remove configuration and state
+if [ "$START_CLEAN" == "true" ]; then
+  echo "Cleaning UM State"
+  rm -f `find $UM_HOME/ -name *.jks`
+  rm -rf $UM_HOME/server/$REALM/data/
+fi
 
 # The first time the server is run, the data will be a total blank slate. We can live with that, except we want to restore the default *@*
 # full ACL.
 if [[ ! -e server/umserver/data ]]
 then
-	echo '*@*' > server/umserver/bin/secfile.conf
-	SERVER_OPTS="$SERVER_OPTS 
-	-DSECURITYFILE=server/umserver/bin/secfile.conf"
+	echo '*@*' > server/$REALM/bin/secfile.conf
 fi
 
-####
-# Starting the realm server in the container in foreground
-####
-java $SERVER_OPTS com.pcbsys.nirvana.server.Server $@ &
+# Create a new service based on the default script that starts UM in the background while keeping it compatible with Command Central start/stop abilities
+START_SCRIPT=$UM_HOME/server/umserver/bin/umservice
+if [ ! -f $START_SCRIPT ] ; then
+   echo "Creating startup script"
+   cp -rf $UM_HOME/server/umserver/bin/nserver $START_SCRIPT
+   sed -i.bak -e '/FIXED_COMMAND=console/ s/^#*/#/' $START_SCRIPT
+fi
+
+# start UM
+/bin/sh $START_SCRIPT start
+
+echo "UM process started. Waiting ..."
+# wait until TC server.log comes up
+while [  ! -f $SAG_HOME/UniversalMessaging/server/$REALM/data/nirvana.log ]; do
+    tail $SAG_HOME/UniversalMessaging/server/$REALM/bin/UMRealmService.log
+    sleep 5
+done
 
 # Ensure that 'docker stop' performs a clean server shutdown
-export SERVER_PID=$!
-trap "rm server/umserver/data/RealmServer.lck; wait $SERVER_PID" SIGTERM
-wait $SERVER_PID
+#export SERVER_PID=$(ps aux | grep nirvana | grep -v grep | awk '{print $2}')
+#trap "rm $SAG_HOME/UniversalMessaging/server/$REALM/data/RealmServer.lck; wait $SERVER_PID" SIGTERM
+
+# this is our main container process
+echo "UM is ONLINE"
+tail -f $SAG_HOME/UniversalMessaging/server/$REALM/data/nirvana.log
