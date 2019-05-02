ab06dfd1b45eac45177d83ee987b2c8fda6a4392
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2019-05-02 17:56:52 -0400

    update the bash profile file to include the newly installed CLI in the PATH

--- scripts/provision_ccnode.sh
+++ scripts/provision_ccnode.sh
@@ -14,7 +14,7 @@ $ANT_CMD -Denv.CC_BOOT=$CC_BOOT -Denv.CC_PASSWORD=$CC_PASSWORD -Dinstall.dir=$SP
 # once done, make sure to run this script to install SPM as a service
 sudo sh $SPM_INSTALL_DIR/bin/afterInstallAsRoot.sh
 
-#source the profile file now it's been updated with CC client bin in PATH
-. $HOME/.sag/profile
-
-## we may want to add that to the .bashrc file for auto sourcing on login
+#update the bash profile file to include the newly installed CLI in the PATH
+echo "CC_CLI_HOME=${SPM_INSTALL_DIR}/CommandCentral/client" >> ${HOME}/.bashrc
+echo "export PATH=\$PATH:\${CC_CLI_HOME}/bin" >> ${HOME}/.bashrc
+. ${HOME}/.bashrc
