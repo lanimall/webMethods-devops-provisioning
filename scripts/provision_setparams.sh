42110a7ac7d11a511829e7a6c84926f86ddefd0c
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2019-09-10 14:48:50 -0400

    fix

--- scripts/provision_setparams.sh
+++ scripts/provision_setparams.sh
@@ -11,13 +11,24 @@ if [ -f ${BASEDIR}/scripts/conf/setenv_webmethods_provisioning.sh ]; then
     . ${BASEDIR}/scripts/conf/setenv_webmethods_provisioning.sh
 fi
 
-PROVIONING_KEY=$1
+PROVISIONING_STACK=$1
 PARAM_NAME=$2
 PARAM_VALUE=$3
+APPEND_EXISTING=$4
 
-PROVISION_SCRIPT_FILENAME_NOEXT="provision_${PROVIONING_KEY}"
+if [ "x$APPEND_EXISTING" = "x" ]; then
+    APPEND_EXISTING="false"
+fi
+
+PROVISION_SCRIPT_FILENAME_NOEXT="provision_${PROVISIONING_STACK}"
 SETENV_FILE_PARAMS="~/setenv-${PROVISION_SCRIPT_FILENAME_NOEXT}.sh"
-CMD="echo \"export ${PARAM_NAME}=${PARAM_VALUE}\" > ${SETENV_FILE_PARAMS}"
+
+FILE_REDIRECTION=">"
+if [ "$APPEND_EXISTING" = "true" ]; then
+    FILE_REDIRECTION=">>"
+fi
+
+CMD="echo \"export ${PARAM_NAME}=${PARAM_VALUE}\" ${FILE_REDIRECTION} ${SETENV_FILE_PARAMS}"
 
 ##become target user for command
 $BASEDIR/scripts/utils/runas_cmd.sh $RUN_AS_USER "$CMD"
