bf85323e9dca4cff9986956dd59a77814dcc4b1d
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2019-09-10 10:00:01 -0400

    re-factoring

---
+++ scripts/internal/provision_um.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## getting current filename and basedir
+THIS=`basename $0`
+THIS_NOEXT="${THIS%.*}"
+THISDIR=`dirname $0`; THISDIR=`cd $THISDIR;pwd`
+BASEDIR="$THISDIR/../.."
+
+## apply global env
+if [ -f ${BASEDIR}/scripts/conf/setenv_webmethods_provisioning.sh ]; then
+    . ${BASEDIR}/scripts/conf/setenv_webmethods_provisioning.sh
+fi
+
+## apply cce env
+if [ -f ${HOME}/setenv-cce.sh ]; then
+    . ${HOME}/setenv-cce.sh
+fi
+
+if [ "x$TARGET_HOST" = "x" ]; then
+    echo "error: variable TARGET_HOST is required...exiting!"
+    exit 2;
+fi
+
+if [ "x$LICENSE_KEY_ALIAS1" = "x" ]; then
+    echo "error: Variable LICENSE_KEY_ALIAS1 (for UM) is required...exiting!"
+    exit 2;
+fi
+
+##### apply um template
+$SAGCCANT_CMD -Denv.CC_CLIENT=$CC_CLIENT \
+              -Denv.CC_TEMPLATE=um-layer/tpl-server.yaml \
+              -Denv.CC_TEMPLATE_ENV=um \
+              -Denv.CC_TEMPLATE_ENV_LICENSE_KEY_ALIAS1=$LICENSE_KEY_ALIAS1 \
+              -Denv.CC_TEMPLATE_ENV_TARGET_HOST=$TARGET_HOST \
+              -Denv.CC_TEMPLATE_ENV_TYPE=server \
+              -Denv.SOCKET_CHECK_TARGET_HOST=$TARGET_HOST \
+              -Denv.SOCKET_CHECK_TARGET_PORT=22 \
+              setup
+
+runexec=$?
+exit $runexec;
--- scripts/provision_tc.sh
+++ scripts/provision_tc.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
 
-SAGCCANT_CMD="sagccant"
-CC_CLIENT=default
+## getting current filename and base path
+THIS=`basename $0`
+THIS_NOEXT="${THIS%.*}"
+THISDIR=`dirname $0`; THISDIR=`cd $THISDIR;pwd`
+BASEDIR="$THISDIR/.."
 
-## apply env
-if [ -f ${HOME}/setenv-cce.sh ]; then
-    . ${HOME}/setenv-cce.sh
-fi
+$BASEDIR/scripts/utils/provision_wrapper.sh $THIS_NOEXT "$@"
+runexec=$?
 
-if [ "x$TARGET_HOST" = "x" ]; then
-    echo "error: variable TARGET_HOST is required...exiting!"
-    exit 2;
-fi
-
-if [ "x$LICENSE_KEY_ALIAS1" = "x" ]; then
-    echo "error: Variable LICENSE_KEY_ALIAS1 (for Terracotta) is required...exiting!"
-    exit 2;
-fi
-
-##### apply um template
-$SAGCCANT_CMD -Denv.CC_CLIENT=$CC_CLIENT \
-              -Denv.CC_TEMPLATE=tc-layer/tpl-server.yaml \
-              -Denv.CC_TEMPLATE_ENV=tc \
-              -Denv.CC_TEMPLATE_ENV_LICENSE_KEY_ALIAS1=$LICENSE_KEY_ALIAS1 \
-              -Denv.CC_TEMPLATE_ENV_TARGET_HOST=$TARGET_HOST \
-              -Denv.CC_TEMPLATE_ENV_TYPE=server \
-              -Denv.SOCKET_CHECK_TARGET_HOST=$TARGET_HOST \
-              -Denv.SOCKET_CHECK_TARGET_PORT=22 \
-              setup
-
-##create/update a file in tmp to broadcast that the script is done
-filename=$0
-filename="${filename%.*}"
-touch /tmp/$filename.done.status
-
-exit 0;
+exit $runexec
